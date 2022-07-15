5d02236237c9e7bbe97a1d02a1a6931999829307
Anthony Twesigye <twesigye10@gmail.com>
2022-07-15 09:32:58 +0300

    converted code to a function

--- R/create_folder_structure.R
+++ R/create_folder_structure.R
@@ -1,12 +1,17 @@
 ## create sub directories for the project
 
-sub_directories <- list("outputs", "inputs", "R", "support_files")
-
-for(x in sub_directories){
-  check_folder <- dir.exists(paste0(getwd(),"/",x))
-  
-  if (check_folder == FALSE) {
-    dir.create(paste0("./",x), showWarnings = FALSE)
+create_folder_structure <- function(input_sub_folders = c("outputs", "inputs", "R", "support_files")) {
+  sub_directories <- input_sub_folders
+  # iterate the subfolders and create them
+  for(x in sub_directories){
+    # check folder existence
+    check_folder <- dir.exists(paste0(getwd(),"/",x))
+    
+    if (check_folder == FALSE) {
+      dir.create(paste0("./",x), showWarnings = FALSE)
+      message(paste("Folder created :", x))
+    } else{
+      warning(paste("Folder already exists :", x))
+    }
   }
-  
 }
